5a33b8b6549c90570cce64d22a57d929fefd692b
Kabir Oberai <oberai.kabir@gmail.com>
2024-04-26 17:33:29 -0400

    Initial Commit

---
+++ Yelpepperoni/YelpepperoniApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpepperoniApp.swift
+//  Yelpepperoni
+//
+//  Created by Kabir Oberai on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct YelpepperoniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
